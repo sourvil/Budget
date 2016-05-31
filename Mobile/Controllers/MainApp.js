00d2198f99215315c05a6c9837d07f98038b084d
Burak <burak.donbay@gmail.com>
2016-06-01 00:22:37 +0300

    NG added

---
+++ Mobile/Controllers/MainApp.js
@@ -0,0 +1,17 @@
+﻿angular.module("MainApp", [])
+
+.controller("MainController", function ($scope, $http) {
+    $scope.surname = "donbay";
+    $scope._surname2 = "donbay2";
+    $scope.teams = [{ 'name': "Fenerbahçe", 'colors': 'Yellow-Navy' }, { 'name': 'Galatasaray', 'colors': 'Yellow-Red' }, {'name':'Beşiktaş','colors' : 'White-Black'}];
+
+    function setSurname(surname)
+    {
+        console.log("SetSurname is called");
+        console.log(surname);
+    };
+
+    $scope.setSurname = setSurname;
+
+});
+
